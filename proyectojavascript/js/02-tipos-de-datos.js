2d9836cda43547a0f98a4a8d5d08cdbf77392741
fullstack <lrizk>
2023-10-10 14:22:39 +0200

    proyectopersonal

--- proyectojavascript/js/02-tipos-de-datos.js
+++ proyectojavascript/js/02-tipos-de-datos.js
@@ -32,3 +32,89 @@ let ejstring3 = "garaje"
 console.log("metodo length:",ejstring1.length);
 console.log("metodo indexof:",ejstring2.indexOf("hadas"));
 console.log("metodo includes:",ejstring3.includes("hadas"));
+
+
+let numero1 = 100;
+let numero2 = 200.25;
+let numero3 = "300";
+
+console.log("NUMERO 1:", numero1);
+console.log("NUMERO 2:", numero2);
+
+console.log("TYPEOF NUMERO 1:", typeof numero1);
+console.log("TYPEOF NUMERO 2:", typeof numero2);
+console.log("TYPEOF NUMERO 3:", typeof numero3);
+
+
+console.log("numero1 * numero2:",numero1*numero2);
+console.log("numero2 - numero1:",numero2-numero1);
+console.log("numero1 + numero2:",numero1+numero2);
+console.log("numero2 / numero1:",numero2/numero1);
+console.log("numero1 % numero2:",numero1%numero2);
+
+
+let resultado = Math.random();
+
+console.log("resultado random:",resultado);
+
+resultado = Math.round(Math.random()*30);
+console.log("numero aleatorio entero:",resultado);
+
+let diametro = 5;
+
+let area;
+
+area = Math.PI*Math.pow(diametro/2,2);
+
+console.log("resultado area:",area);
+
+let dado = Math.round(1+Math.random()*5);
+console.log("resultado dado:",dado);
+
+let resultadoOrden = 20 + 30 * 2;
+console.log("orden de operaciones:",resultadoOrden);
+
+let resultadoOrden2 = (20+30) * 2;
+console.log("orden operaciones 2:",resultadoOrden2);
+
+
+let prenda1=120;
+let prenda2=85;
+let prenda3=24;
+
+
+let importe = prenda1+prenda2+prenda3;
+
+console.log("importe sin ahorro:",importe);
+
+let descuento = 40;
+
+let ahorro=importe*descuento/100;
+
+console.log("ahorro:",ahorro);
+
+let total=importe-ahorro;
+
+console.log("total a pagar:",total);
+
+let meGusta =0;
+
+meGusta+=15;
+
+meGusta-=6;
+console.log("me gusta:",meGusta);
+
+let nombre = "lorena";
+let email = "lorena@gmail.com";
+
+console.log("nombre: " + nombre + " email: " + email);
+
+
+
+
+
+
+
+
+
+
