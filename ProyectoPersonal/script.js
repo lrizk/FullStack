0633de1292a825b17771aba680a0d1c929015073
fullstack <lrizk>
2023-11-10 12:52:32 +0100

    proyectopersonal

---
+++ ProyectoPersonal/script.js
@@ -0,0 +1,8 @@
+document.getElementById('mostrarPolitica').addEventListener('click', function(){
+    document.getElementById('popup').style.display = 'block';
+  });
+  
+  function cerrarPopup() {
+    document.getElementById('popup').style.display = 'none';
+  }
+  
---
+++ proyectojavascript/js/12-dom.js
@@ -0,0 +1,102 @@
+*/
+const heading = document.querySelector(".seccionPrincipal h1");//RETORNA 0 O 1 ELEMENTO
+console.log(heading); // NOS MUESTRA POR PANTALLA NUESTRA SELECCION
+
+/*
+!!!!!!!!!!!!
+EJERCICIO
+SELECCIONAR EL ID DEL BOTON QUE HEMOS CREADO
+
+PARA MODIFICAR EL CONTENIDO HACEMOS USO DE LA PROPIEDAD .textContent
+    */
+heading.textContent="Nuevo Titulo 1234";
+
+
+heading.classList.add("NuevaClaseDesdJS"); //AÑADIR
+
+heading.classList.remove("NuevaClaseDesdJS"); //ELIMINAR
+
+heading.classList.add("fondoRojo")
+
+
+const enlacesJS = document.querySelector("div a");
+console.log(enlacesJS);//SOLO MUESTRA EL PRIMERO
+// ES UNA PROPIEDAD DE QUERYSELECTOR, ME MUESTRA UN ELEMENTO O NINGUNO
+
+//QUERYSELECTORALL
+
+const enlacesJS2 = document.querySelectorAll("div a");
+console.log(enlacesJS2);
+//ME MUESTRA TODOS LOS ENLACES QUE CONCUERDAN CON LA SELECCIÓN
+//ME LO MUESTRA EN UN NODELIST, PARA ACCEDER A CADA ELEMENTO, LO HACEMOS
+//AL IGUAL QUE LOS ARRAYS
+console.log(enlacesJS2[0]);
+
+//OTRA PROPIEDAD QUE PODEMOS MODIFICAR ES .href
+enlacesJS2[0].href="https://google.com"
+
+//EJERCICIO
+//MODIFICAR EL CONTENIDO Y EL HREF DEL SEGUNDO ENLACE UNICAMENTE CON QUERYSELECTORALL
+//PONER CONTENIDO MODIFICADO CON QUERYSELECTORALL. Y LLEVAR A OTRO ENLACE EXTERNO
+
+
+enlacesJS2[1].textContent = "Enlace2 Modificado";
+enlacesJS2[1].href="https://google.com"
+
+//getElementById
+
+const botonJS = document.getElementById("botonPrincipal");
+console.log(botonJS);
+
+//TODO LO VISTO ANTERIORMENTE PARA AÑADIR/MODIFICAR/ELIMINAR PROPIEDADES ES APLICABLE EN GETELEMENTBYID
+
+//GETELEMENTBYCLASSNAME
+
+const botonJsClass =document.getElementsByClassName("botonClase");
+
+
+**********************************************
+VEREMOS COMO GENERAR CODIGO EN HTML DESDE JAVASCRIPT
+UN ESTADO NUEVO DE FACEBOOK SE GENERA POR JAVASCRIPT
+
+*/
+
+const nuevoEnlace = document.createElement("A");
+
+//NOS PERMITE CREAR CODIGO HTML
+//JAVASCRIPT RECOMIENDA USAR MAYUSCULAS CUANDO USAMOS CREATE
+// DIV, A , P, IMG
+
+//AGREGAR HREF
+
+nuevoEnlace.href = "#";
+
+//AGREGAR TEXTO
+
+nuevoEnlace.textContent = "Nuevo Enlace4";
+
+//AGREGAR CLASE
+
+nuevoEnlace.classList.add("enlace");//LE AÑADO LA CLASE DE INFORMATE
+
+console.log(nuevoEnlace)
+
+//AGREGAMOS AL DOCUMENTO
+
+const enlacesJS3 =document.querySelector(".navPrincipal");//PARA ELLO SELECCIONAMOS EL ELEMENTO PADRE
+enlacesJS3.appendChild(nuevoEnlace)//APPENCHILD ME PERMITE AGREGAR ELEMENTOS HIJOS
+
+
+const ejercicioEnlace = document.createElement("A");//NOS PERMITE CREAR CODIGO HTML
+//JAVASCRIPT RECOMIENDA USAR MAYUSCULAS CUANDO USAMOS CREATE
+//AGREGAR HREF
+ejercicioEnlace.href = "#";
+//AGREGAR TEXTO
+ejercicioEnlace.textContent = "Ejercicio Enlace";
+//AGREGAR CLASE
+ejercicioEnlace.classList.add("enlaces");//LE AÑADO LA CLASE
+//AGREGAMOS AL DOCUMENTO
+//LO QUEREMOS AGREGAR A LA DERECHA DEL BOTON INFORMATE
+
+const selectorEjercicioEnlace =document.querySelector(".navPrincipal");//PARA ELLO SELECCIONAMOS EL ELEMENTO PADRE
+selectorEjercicioEnlace.appendChild(ejercicioEnlace)//APPENCHILD ME PERMITE AGREGAR ELEMENTOS HIJOS
